8d818838c41bc81bde053a4c8c76971516d70a12
Rodrigo Matagawa devmatsu <devmatsu@hotmail.com>
2025-01-15 16:16:08 -0300

    feat: initialize SST project with configuration and source files

---
+++ sst.config.ts
@@ -0,0 +1,23 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "empath-challenge",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    const api = new sst.aws.ApiGatewayV2("MyApi");
+
+    api.route("GET /", {
+      handler: "src/lambda.handler",
+    });
+
+    return {
+      url: api.url,
+    };
+  },
+});
